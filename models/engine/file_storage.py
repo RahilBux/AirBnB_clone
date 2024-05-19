e4d3a3253601df869f078bb84953dbf853c49318
RahilBux <rahilbux@gmail.com>
2024-05-19 17:04:00 +0200

    added the User class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -40,8 +40,10 @@ class FileStorage:
     def classes(self):
         """Returns a dict of classes that are valid"""
         from models.base_model import BaseModel
+        from models.user import User
 
-        classes = {"BaseModel": BaseModel}
+        classes = {"BaseModel": BaseModel,
+                   "User": User}
         return classes
 
     def attributes(self):
@@ -50,6 +52,11 @@ class FileStorage:
                 "BaseModel":
                 {"id": str,
                     "created_at": datetime.datetime,
-                    "updated_at": datetime.datetime}
-                    }
+                    "updated_at": datetime.datetime},
+                "User":
+                {"email": str,
+                    "password": str,
+                    "first_name": str,
+                    "last_name": str}
+                }
         return attributes
